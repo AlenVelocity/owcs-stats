50f94781e82b2d96f64ae2aa058dae8c13c79a9d
AlenVelocity <alenyohannan71@gmail.com>
2024-08-18 01:09:18 +0530

    chore(): update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				hostname: 'assets.faceit-cdn.net'
+			},
+			{
+				hostname: 'distribution.faceit-cdn.net'
+			}
+		]
+	}
+}
 
-export default nextConfig;
+export default nextConfig
